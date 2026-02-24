92214005aa57fd4f19b98248f6fb92a66b80af12
Suryansh Rastogi <srbrothers1340@gmail.com>
2026-02-24 18:31:05 +0530

    added route for fetch

--- Backend/Routes/mealsRouter.js
+++ Backend/Routes/mealsRouter.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const ensureAuth = require("../Middlewares/authMiddleware");
-const { saveMeal } = require("../Controllers/mealsController");
+const { saveMeal } = require("../Controllers/saveMealController");
+const { fetchMeals } = require("../Controllers/fetchMealsController");
 
 router.post("/save", ensureAuth, saveMeal);
-
+router.get("/fetch", ensureAuth, fetchMeals);
 module.exports = router;
