da8213433dfdfb16b9b5c3cfaaf76667f41072a0
Suryansh Rastogi <srbrothers1340@gmail.com>
2026-03-05 03:58:53 +0530

    changes made due to removing the frontend involved in sending the mail

--- Backend/src/controllers/mail.controller.js
+++ Backend/src/controllers/mail.controller.js
@@ -1,13 +1,11 @@
 const {mailSender} = require('../services/mail.service');
 
-const mailController = async (req, res) => {
+const mailController = async (email, verificationToken) => {
     try {
-        const { email, verificationToken } = req.body;
         await mailSender(email, verificationToken);
-        res.status(200).json({ message: "Email sent successfully" });
     } catch (err) {
         console.log(err);
-        res.status(400).json({ message: err.message });
+        throw new Error("Failed to send verification email");
     }
 };
 
