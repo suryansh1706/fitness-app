0e1b5697fb31773e0a55e23392148d931438eb48
Suryansh Rastogi <srbrothers1340@gmail.com>
2026-03-23 00:37:09 +0530

    fixed bugs

--- Backend/src/services/user.service.js
+++ Backend/src/services/user.service.js
@@ -1,13 +1,14 @@
 const Profile = require('../models/profile.model');
 
 const saveProfile = async (userId, profileData) => {
-    const { age, weight, height, activityLevel } = profileData;
+    const { age, weight, height, activityLevel, goal } = profileData;
 
     const profile = new Profile({
         age,
         weight,
         height, 
         activityLevel,
+        goal,
         createdBy: userId
     });
     await profile.save();
